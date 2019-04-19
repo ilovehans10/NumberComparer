b650ca6596e0323f499a2aa3ec3548311441ef44
Hans Larsson <hanandlia@gmail.com>
2019-04-19 13:00:49 -0500

    initial compairer

--- Compairer.rb
+++ Compairer.rb
@@ -1,6 +1,10 @@
 class Integer
   def len
-    Math.log10(self).to_i + 1
+    if self == 0
+      return 0
+    else
+      return Math.log10(self).to_i + 1
+    end
   end
 end
 
@@ -8,17 +12,28 @@ def compairer(int1,int2)
   int1len = int1.len
   int2len = int2.len
 
-  if int1len == 0
+  if int1len == 0 or int2len == 0
     return 0
-  elsif int1len > int2len
-    return int1len
-  elsif int1len < int2len
-    return int2len
+  elsif int1len != int2len
+    if int1len < int2len
+      puts "AAAA"
+      return int1len
+    elsif int1len > int2len
+      puts "BBBB"
+      return int2len
+    end
+  else
+    puts int1.digits.first
+    puts int2.digits.first
+    if int1.digits.reverse.first == int2.digits.reverse.first
+      puts "CCCC"
+      return int1len
+    end
   end
 end
 
-puts compairer(10,2)
-puts compairer(10,20)
-puts compairer(20,20)
-puts compairer(23,20)
-puts compairer(22,23)
+puts "1: " + compairer(10, 2 ).to_s #1
+puts "2: " + compairer(10, 20).to_s #2
+puts "3: " + compairer(20, 20).to_s #0
+puts "4: " + compairer(23, 20).to_s #1
+puts "5: " + compairer(22, 23).to_s #1
